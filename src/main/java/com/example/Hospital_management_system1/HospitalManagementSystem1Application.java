a28438793f8db3a0672dde866b033978a046945f
wakku9780@gmail.com <wakku9780@gmail.com>
2023-10-16 01:56:06 +0530

    hgfd

---
+++ src/main/java/com/example/Hospital_management_system1/HospitalManagementSystem1Application.java
@@ -0,0 +1,13 @@
+package com.example.Hospital_management_system1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystem1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hospital_management_system1/HospitalManagementSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hospital_management_system1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
